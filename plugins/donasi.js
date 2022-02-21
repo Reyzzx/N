7ab7d777787ace4d2e17415a7a97ea04f98380ab
Rey <99199008+fvckreyz@users.noreply.github.com>
2022-02-21 00:47:01 -0800

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
+┌〔 Donasi • berkah selalu 〕
+├ Saweria : https://saweria.co/ReyXD
+├ Gopay : 082138746029
+└────
+`.trim(), footer, 'Owner', '.owner')
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
